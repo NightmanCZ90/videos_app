0d83eb6beaa37d873793a3f5aa8954a453e32797
NightmanCZ90 <novak.jan90@gmail.com>
2020-11-13 00:23:13 +0100

    added mark up and onInputChange method to SearchBar component

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,10 +1,29 @@
 import React from 'react'
 
 class SearchBar extends React.Component {
+  state = { term: '' };
+
+  onInputChange = (e) => {
+    e.preventDefault();
+    this.setState({
+      term: e.target.value
+    })
+  };
 
   render() {
     return (
-      <div>SearchBar</div>
+      <div className="search-bar ui segment">
+        <form className="ui form">
+          <div className="field">
+            <label>Video Search</label>
+            <input 
+              type="text" 
+              value={this.state.term} 
+              onChange={this.onInputChange}
+            />
+          </div>
+        </form>
+      </div>
     )
   }
 };
