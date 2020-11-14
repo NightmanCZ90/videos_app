f0c23a703a6aeaee7f78517df5c38de7f7756805
NightmanCZ90 <novak.jan90@gmail.com>
2020-11-14 22:26:54 +0100

    added styling to VideoItem component

--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -1,8 +1,16 @@
+import './VideoItem.css';
 import React from 'react';
 
-const VideoItem = (props) => {
+const VideoItem = ({ video }) => {
   return (
-    <div>Video Item</div>
+    <div className="video-item item" >
+      <img className="ui image" src={video.snippet.thumbnails.medium.url} />
+      <div className="content" >
+        <div className="header">
+          {video.snippet.title}
+        </div>
+      </div>
+    </div>
   )
 };
 
