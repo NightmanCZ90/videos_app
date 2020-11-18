d0da578a5a05f42475a2dec5b6db473884c9e494
NightmanCZ90 <novak.jan90@gmail.com>
2020-11-18 22:56:12 +0100

    created and implemented custom hook useVideos

--- src/components/App.js
+++ src/components/App.js
@@ -2,30 +2,19 @@ import React, { useState, useEffect } from 'react';
 import SearchBar from './SearchBar';
 import VideoDetail from './VideoDetail';
 import VideoList from './VideoList';
-import youtube from '../apis/youtube.js';
+import useVideos from '../hooks/useVideos';
 
 const App = () => {
-  const [videos, setVideos] = useState([]);
   const [selectedVideo, setSelectedVideo] = useState(null);
+  const [videos, search] = useVideos('alien worlds');
 
   useEffect(() => {
-    onTermSubmit('space');
-  }, [])
-
-  const onTermSubmit = async (term) => {
-    const response = await youtube.get('/search', {
-      params: {
-        q: term
-      }
-    });
-    
-    setVideos(response.data.items);
-    setSelectedVideo(response.data.items[0]);
-  };
-
+    setSelectedVideo(videos[0]);
+  }, [videos]);
+  
   return (
     <div className="ui container">
-      <SearchBar onFormSubmit={onTermSubmit} />
+      <SearchBar onFormSubmit={search} />
       <div className="ui grid">
         <div className="ui row">
           <div className="eleven wide column">
